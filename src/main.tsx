5781a8ddd6793e279f0ab0cbc55e082b034896b5
Danilo Almeida <danilotecteatro@yahoo.com.br>
2025-08-08 19:21:49 -0300

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Elemento root não encontrado');
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
